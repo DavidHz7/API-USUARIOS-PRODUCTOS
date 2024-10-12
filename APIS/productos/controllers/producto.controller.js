bb7768fbc2443db3da1957771579559c96569857
DavidHz7 <lddavidhernandez@gmail.com>
2024-10-12 10:02:07 -0500

    agregando consultas

--- APIS/productos/controllers/producto.controller.js
+++ APIS/productos/controllers/producto.controller.js
@@ -1,24 +1,101 @@
 const {response, request} = require('express');
+const { PrismaClient } = require('@prisma/client');
 
-    const viewproducto = async (req = request, res = response) => {
+const prisma = new PrismaClient();
+
+const viewproducto = async (req = request, res = response) => {
     res.json({
         'msg':'puto d'
     })
 };
 
+const addProduct = async (req = request, res = response) => {
+
+    const {name, precio, cantidad, fecha_exp} = req.body;
 
 
-const productoGet = (req = request, res = response) => {
+    const result = await prisma.productos.create({
+        data:{
+            name, 
+            precio, 
+            cantidad, 
+            fecha_exp
+        }
+    }).catch((e)=>{
+        return e.message;
+    }).finally(async () => {
+        await prisma.$disconnect();
+    });
+
+    res.json(result);
+};
 
 
-    //ejemplo de respuesta tipo json
-    res.status(200).json(
+const Showproductos = async(req=request, res=response)=>{
+
+    const productos = await prisma.productos.findMany()
+    .catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
+    res.json({
         productos
-    );
-}
+    });
+};
+
+const Editproductos = async(req=request, res=response)=>{
+    const { id } = req.params;
+
+    const {name, precio, cantidad, fecha_exp} = req.body;
+
+    const result = await prisma.productos.update({
+        where:{
+            id: Number(id)
+        },
+        data: {
+            name, 
+            precio, 
+            cantidad, 
+            fecha_exp
+        }
+    }).catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
+    res.json({
+        result
+    });
+
+};
+
+const Deleteproductos = async(req=request, res=response)=>{
+    const { id } = req.params;
+
+    const result = await prisma.productos.delete({
+        where:{
+            id: Number(id)
+        }
+    }).catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
+    res.json({
+        result
+    });
+};
+
 
 module.exports={
     viewproducto,
-    productoGet,
+    Deleteproductos,
+    Editproductos,
+    Showproductos,
+    addProduct
     
 }
--- APIS/productos/routes/producto.route.js
+++ APIS/productos/routes/producto.route.js
@@ -1,13 +1,16 @@
 
 const { Router } = require('express');
-const {viewproducto, productoGet} = require('../controllers/producto.controller');
+const {viewproducto, Deleteproductos, Editproductos,Showproductos,addProduct} = require('../controllers/producto.controller');
 // Ruta para obtener una lista de productos
 const routerprod = Router();
 
 
 
 routerprod.get('/',viewproducto);
-routerprod.get('/lista',productoGet);
+routerprod.get('/lista',Showproductos);
+routerprod.post('/',addProduct);
+routerprod.put('/:id',Editproductos);
+routerprod.delete('/:id',Deleteproductos);
 
 
 module.exports = routerprod;
